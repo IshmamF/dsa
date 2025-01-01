9d5677831476078e623b34aef4173221bcb8a3b1
IshmamF <ishmamf2003@gmail.com>
2025-01-01 13:53:40 -0500

    added neetcode bottom up approach and drew visualization

--- algorithms/dp/ones-and-zeros/ones-and-zeros.py
+++ algorithms/dp/ones-and-zeros/ones-and-zeros.py
@@ -6,6 +6,8 @@ We need to decide if we want to include a string or not include a string, which 
 Possible base cases is if either m or n are less than or equal to 0, and if we reach the end of the list
 
 '''
+
+# My Solution
 from collections import Counter
 def findMaxForm(strs, m, n):
     # i for index
@@ -42,6 +44,31 @@ strs = ["10","0001","111001","1","0"]
 m, n = 5, 3
 output = findMaxForm(strs, m, n)
 print(output)
+
+strs = ["10","0","1"]
+m, n = 1, 1
+output = findMaxForm(strs, m, n)
+print(output)
+
+# Neetcode Bottom Up Approach
+
+def findMaxForm(strs, m, n):
+    arr = [[0] * 2 for _ in range(len(strs))]
+    for i, s in enumerate(strs):
+        for c in s:
+            arr[i][ord(c) - ord('0')] += 1
+
+    dp = [[[0] * (n + 1) for _ in range(m + 1)] for _ in range(len(strs) + 1)]
+    print(dp)
+
+    for i in range(1, len(strs) + 1):
+        for j in range(m + 1):
+            for k in range(n + 1):
+                dp[i][j][k] = dp[i - 1][j][k]
+                if j >= arr[i - 1][0] and k >= arr[i - 1][1]:
+                    dp[i][j][k] = max(dp[i][j][k], 1 + dp[i - 1][j - arr[i - 1][0]][k - arr[i - 1][1]])
+
+    return dp[len(strs)][m][n]
 
 strs = ["10","0","1"]
 m, n = 1, 1
