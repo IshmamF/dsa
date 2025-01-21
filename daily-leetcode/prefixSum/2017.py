943cacd5872a0488e67cd9b0d58f1859aaf7d08b
IshmamF <ishmamf2003@gmail.com>
2025-01-21 09:56:26 -0500

    grid game more optimal solution

--- daily-leetcode/prefixSum/2017.py
+++ daily-leetcode/prefixSum/2017.py
@@ -26,6 +26,23 @@ class Solution:
                 robotMin = min(maxVal, robotMin, grid[1][gridLength-2])
         return robotMin
 
-            
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        gridLength = len(grid[0])
+        if gridLength == 1:
+            return 0
+        elif gridLength == 2:
+            return min(grid[0][1], grid[1][0])
 
+        totalSum = sum(grid[0])
+        curr = 0
+
+        robotMin = totalSum
+        for i in range(0, gridLength):
+            totalSum -= grid[0][i]
+            currMax = max(totalSum, curr)
+            curr += grid[1][i]
+            robotMin = min(robotMin, currMax)
+
+        return robotMin
         
