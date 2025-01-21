e7070a6cfd005341181c59168fd6a17bbef33a98
IshmamF <ishmamf2003@gmail.com>
2025-01-20 19:08:44 -0500

    reshape matrix sol 2

--- code-signal-practice/reshape-matrix.py
+++ code-signal-practice/reshape-matrix.py
@@ -17,3 +17,28 @@ class Solution:
                     res.append(colVals.copy())
                     colVals = []
         return res
+    
+
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        '''
+
+        '''
+        ROW, COL = len(mat), len(mat[0])
+        if r * c != ROW * COL:
+            return mat
+        oneDArray = []
+
+        for i in range(ROW):
+            for j in range(COL):
+                oneDArray.append(mat[i][j])
+
+        res = [[0]*c for _ in range(r)]
+
+        ptr = 0
+        for i in range(r):
+            for j in range(c):
+                res[i][j] = oneDArray[ptr]
+                ptr += 1  
+
+        return res           
